bffc13c86dd0bc4e0bd7d034cbe85bdc887e42f1
Cheuk (Chuck) Chow <rosarollins069@gmail.com>
2025-09-26 17:04:51 -0400

    Update interrupts.cpp

--- interrupts.cpp
+++ interrupts.cpp
@@ -16,3 +16,44 @@ public:
 
 
 
+template<typename Cover>
+class protect_lock {
+    Cover& m_c;
+public:
+    protect_lock(Cover& c) : m_c(c) { c.protect(); }
+    ~protect_lock() { m_c.unprotect(); }
+};
+
+template<typename Cover>
+class sync_lock {
+    Cover& m_c;
+public:
+    sync_lock(Cover& c) : m_c(c) { c.sync(); }
+    ~sync_lock() { m_c.unsync(); }
+};
+
+
+//the below is viable on an ARM Cortex-M4 microcontroller
+
+using Cover = cover<armv7_m::cover>;
+static Cover cov;
+static unsigned count;
+
+extern "C" void SysTickHandler(void)
+{
+    sync_lock<Cover> lk(cov);
+    count++;
+}
+
+int main()
+{
+    setupSysTick();
+    while(1) {
+        bool odd;
+        {
+            protect_lock<Cover> lk;
+            odd = (count & 1) != 0;
+        }
+        setLed(odd);
+    }
+}
