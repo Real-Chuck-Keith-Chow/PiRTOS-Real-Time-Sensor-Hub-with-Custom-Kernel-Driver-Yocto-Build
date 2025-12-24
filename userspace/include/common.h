7592661e220094534327efbed2a89abe24e51ce2
Cheuk Fung Keith (Chuck) Chow <rosarollins069@gmail.com>
2025-12-24 11:16:35 -0500

    Add minimal logging helper and clean up common header

--- userspace/include/common.h
+++ userspace/include/common.h
@@ -3,6 +3,8 @@
 
 #include <cstdint>
 #include <chrono>
+#include <iostream>
+#include <string>
 
 struct SensorData {
     float temperature;
@@ -10,10 +12,11 @@ struct SensorData {
     int motion_detected;
     int button_pressed;
     uint64_t timestamp;
-    
-    SensorData() : temperature(0.0), humidity(0.0), 
-                  motion_detected(0), button_pressed(0), 
-                  timestamp(0) {}
+
+    SensorData()
+        : temperature(0.0f), humidity(0.0f),
+          motion_detected(0), button_pressed(0),
+          timestamp(0) {}
 };
 
 enum class LogLevel {
@@ -23,6 +26,15 @@ enum class LogLevel {
     ERROR
 };
 
-void log_message(LogLevel level, const std::string& message);
+inline void log_message(LogLevel level, const std::string& message) {
+    const char* tag = "";
+    switch (level) {
+    case LogLevel::DEBUG:   tag = "DEBUG"; break;
+    case LogLevel::INFO:    tag = "INFO"; break;
+    case LogLevel::WARNING: tag = "WARN"; break;
+    case LogLevel::ERROR:   tag = "ERROR"; break;
+    }
+    std::cout << "[" << tag << "] " << message << std::endl;
+}
 
 #endif // COMMON_H
