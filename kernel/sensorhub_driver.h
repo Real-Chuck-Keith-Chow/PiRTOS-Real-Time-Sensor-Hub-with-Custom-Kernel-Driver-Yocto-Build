b8b2ebfddc899a18381deb1b1b418d47c799145e
Cheuk Fung Keith (Chuck) Chow <rosarollins069@gmail.com>
2025-12-24 11:02:05 -0500

    Sync sensor

--- kernel/sensorhub_driver.h
+++ kernel/sensorhub_driver.h
@@ -3,11 +3,14 @@
 
 #include <linux/ioctl.h>
 
+// Ioctl magic
 #define SENSORHUB_IOC_MAGIC 'S'
 
+// Commands
 #define SENSORHUB_RESET_DATA _IO(SENSORHUB_IOC_MAGIC, 1)
 #define SENSORHUB_GET_STATUS _IOR(SENSORHUB_IOC_MAGIC, 2, int)
 
+// Data structure shared with userspace (SensorManager)
 struct sensorhub_data {
     float temperature;
     float humidity;
